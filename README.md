2fdae3d80a82291361b4b3d9af636c5d780de1cf
Dave Murphy <dave.murphy@plusonerobotcis.com>
2021-07-29 23:19:15 -0600

    added readme content made items persist after reload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Homework week 5 | Work Day Scheduler
+
+This week I made a work day scheduler. The user can enter tasks, meetings, to-do's, etc. in the hourly time slots, hit the save button and the information entered will persist after the web page is reloaded.
+
+## Features 
+
+* The current date is displayed in the header of the web page
+* The user can enter input to each timeslot and that information will persist even after the web page is reloaded
+
+## Screenshot
+
+![Work Day Scheduler](./assets/images/work-day-scheduler.jpg)
+
+## Link to web app
+
+Link to my [Work Day Scheduler](https://murda02.github.io/homework-week-5-dlm/)
+
+## Author
+
+    Dave Murphy
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,115 @@
+// displays the day in the header
 var todaysDate = moment().format("dddd, MMM Do, YYYY");
 $("#currentDay").text(todaysDate);
 
-// column 2 needs to be an input (html)
-// column 3 needs to be a button (html?)
-// then create an eventListener that saves user input (from column 2) to 
-// local storage
+let button8am = document.querySelector("#btn8am");
+let button9am = document.querySelector("#btn9am");
+let button10am = document.querySelector("#btn10am");
+let button11am = document.querySelector("#btn11am");
+let button12pm = document.querySelector("#btn12pm");
+let button1pm = document.querySelector("#btn1pm");
+let button2pm = document.querySelector("#btn2pm");
+let button3pm = document.querySelector("#btn3pm");
+let button4pm = document.querySelector("#btn4pm");
+let button5pm = document.querySelector("#btn5pm");
+
+let input8am = document.querySelector("#input8am");
+let input9am = document.querySelector("#input9am");
+let input10am = document.querySelector("#input10am");
+let input11am = document.querySelector("#input11am");
+let input12pm = document.querySelector("#input12pm");
+let input1pm = document.querySelector("#input1pm");
+let input2pm = document.querySelector("#input2pm");
+let input3pm = document.querySelector("#input3pm");
+let input4pm = document.querySelector("#input4pm");
+let input5pm = document.querySelector("#input5pm");
+
+renderTask();
+
+// event listener for the 8am button, when clicked it stores the
+// value from the 8am GUI input into localstorage
+button8am.addEventListener("click", e => {
+    e.preventDefault();
+    localStorage.setItem("input8am", input8am.value);
+    renderTask();
+
+});
+
+button9am.addEventListener("click", e => {
+    e.preventDefault();
+    localStorage.setItem("input9am", input9am.value);
+    renderTask();
+
+});
+
+button10am.addEventListener("click", e => {
+    e.preventDefault();
+    localStorage.setItem("input10am", input10am.value);
+    renderTask();
+
+});
+
+button11am.addEventListener("click", e => {
+    e.preventDefault();
+    localStorage.setItem("input11am", input11am.value);
+    renderTask();
+
+});
+
+button12pm.addEventListener("click", e => {
+    e.preventDefault();
+    localStorage.setItem("input12pm", input12pm.value);
+    renderTask();
+
+});
+
+button1pm.addEventListener("click", e => {
+    e.preventDefault();
+    localStorage.setItem("input1pm", input1pm.value);
+    renderTask();
+
+});
+
+button2pm.addEventListener("click", e => {
+    e.preventDefault();
+    localStorage.setItem("input2pm", input2pm.value);
+    renderTask();
+
+});
+
+button3pm.addEventListener("click", e => {
+    e.preventDefault();
+    localStorage.setItem("input3pm", input3pm.value);
+    renderTask();
+
+});
+
+button4pm.addEventListener("click", e => {
+    e.preventDefault();
+    localStorage.setItem("input4pm", input4pm.value);
+    renderTask();
+
+});
+
+button5pm.addEventListener("click", e => {
+    e.preventDefault();
+    localStorage.setItem("input5pm", input5pm.value);
+    renderTask();
+
+});
+
+// function that renders the input values from local storage and
+// puts them in the GUI according to time and persists after a 
+// reload
+function renderTask () {
+    input8am.value = localStorage.getItem("input8am");
+    input9am.value = localStorage.getItem("input9am");
+    input10am.value = localStorage.getItem("input10am");
+    input11am.value = localStorage.getItem("input11am");
+    input12pm.value = localStorage.getItem("input12pm");
+    input1pm.value = localStorage.getItem("input1pm");
+    input2pm.value = localStorage.getItem("input2pm");
+    input3pm.value = localStorage.getItem("input3pm");
+    input4pm.value = localStorage.getItem("input4pm");
+    input5pm.value = localStorage.getItem("input5pm");
+}
